9af07654baf0371f5c9be2b4f358cdb72decbe51
ViktorPut <wilgelm1997@gmail.com>
2019-12-07 12:35:57 +0300

    ";eqwte"

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
         //val s : String;
+        //val s : String;
+        //val s : String;
     }
 }
