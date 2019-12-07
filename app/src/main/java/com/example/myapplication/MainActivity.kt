8aeb08172b537ea509ea0e6be0a4a89445e41733
ViktorPut <wilgelm1997@gmail.com>
2019-12-07 12:05:32 +0300

    ";eqwte"

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var btn : Button;
-
+        try {
+            var fac = ArrayIndexOutOfBoundsException().message
+        }catch (e : Exception){
+            println("test");
+        }
     }
 }
